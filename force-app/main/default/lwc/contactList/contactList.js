98644c7fef9fb834330ba9ccefcc31b159e60108
edwinestro <101540460+edwinestro@users.noreply.github.com>
2022-06-26 12:56:45 -0500

    cambios generales

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,15 @@
+import { LightningElement, wire } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import REVENUE_FIELD from '@salesforce/schema/Contact.LastName';
+import INDUSTRY_FIELD from '@salesforce/schema/Contact.Email';
+import getAccounts from '@salesforce/apex/ContactController.getContacts';
+const COLUMNS = [
+    { label: 'Account Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Annual Revenue', fieldName: REVENUE_FIELD.fieldApiName, type: 'text' },
+    { label: 'Industry', fieldName: INDUSTRY_FIELD.fieldApiName, type: 'text' }
+];
+export default class contactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getAccounts)
+    contacts;
+}
---
+++ force-app/main/default/lwc/demoDay/demoDay.js
@@ -0,0 +1,21 @@
+import { LightningElement, wire } from 'lwc';
+import buscar from '@salesforce/apex/POSController.buscar';
+export default class demoDay extends LightningElement {
+    codigo = null; 
+    cantidad = null; 
+    @wire(buscar, { codigo: '$codigo' })
+    items;
+    handleChange(event) {
+        this.codigo = event.detail.value;
+    } 
+    handleChange2(event) {
+        this.cantidad = event.detail.value;
+    }
+    reset() {
+        this.codigo = null;
+    }
+    
+    add() {
+        //To do
+    }
+}
